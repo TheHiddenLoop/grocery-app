8c5506b26ffe8b49fb558d89376933f7b99207a6
Angesh Chauhan <chauhanangesh60@gmail.com>
2026-01-13 18:27:13 +0530

    pages added

---
+++ frontend/app/components/Input.jsx
@@ -0,0 +1,49 @@
+const InputField = ({
+  name,
+  value,
+  onChange,
+  icon: Icon,
+  placeholder,
+  type = "text",
+  className = "",
+  active = false,
+}) => (
+  <div className={`relative mb-4 ${className}`}>
+    {Icon && (
+      <div className="absolute inset-y-0 left-0 pl-4 flex items-center pointer-events-none z-10">
+        <Icon className="h-5 w-5 text-text-secondary" />
+      </div>
+    )}
+
+    <input
+      type={type}
+      name={name}
+      value={value}
+      onChange={onChange}
+      readOnly={active}
+      placeholder={placeholder}
+      className="
+        w-full
+        pl-12
+        pr-4
+        py-3
+        ms:py-3
+        bg-bg-secondary
+        border
+        border-border
+        rounded-xl
+        focus:ring-2
+        focus:ring-primary
+        focus:border-primary
+        transition-skin
+        duration-300
+        text-text-primary
+        placeholder-text-secondary
+        shadow-skin
+        backdrop-blur-sm
+      "
+    />
+  </div>
+);
+
+export default InputField;
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,19 @@
-function page() {
+import { redirect } from "next/navigation";
+
+function Page() {
+  const user = false; 
+
+  if (!user) {
+    redirect("/login"); 
+  }
+
   return (
     <div>
-      <h1 className="text-center text-3xl text-gray-700 bg-green-100 min-h-screen">Hii There</h1>
+      <h1 className="text-center text-3xl text-gray-700 bg-green-100 min-h-screen">
+        Hii There
+      </h1>
     </div>
-  )
+  );
 }
 
-export default page
+export default Page;
