5af44115f85f074a75df49d7b41db943a82e1ce5
Angesh Chauhan <chauhanangesh60@gmail.com>
2026-01-17 19:42:42 +0530

    pages added

---
+++ frontend/app/(auth)/home/page.jsx
@@ -0,0 +1,14 @@
+import Header from "@/app/components/Header";
+import Hero from "@/app/components/Hero";
+
+const Page = () => {
+  return (
+    <section
+      className="relative bg-bg-primary overflow-hidden ">
+      <Header />
+      <Hero />
+    </section>
+  );
+};
+
+export default Page;
